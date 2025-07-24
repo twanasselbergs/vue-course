5d89ee606f4388d620779877a967da3fa5955e60
Garon <garon@vitrion.nl>
2025-07-24 17:30:14 +0200

    Set up Section 5

--- section-5/vue/src/App.vue
+++ section-5/vue/src/App.vue
@@ -1,30 +1,14 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl font-medium">Event Booking App</h1>
+    <h2 class="text-2xl font-medium">All Events</h2>
+    <EventList />
+    <h2 class="text-2xl font-medium">Your Bookings</h2>
+    <BookingList />
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import EventList from "./components/Eventlist.vue";
+import BookingList from "./components/BookingList.vue";
+</script>
