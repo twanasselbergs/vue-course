72c1ca2a914f70798b530dd55bc7fe0fc7b433da
Garon <garon@vitrion.nl>
2025-07-25 17:09:34 +0200

    Update Section 11

--- section-11/vue/src/router/index.ts
+++ section-11/vue/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FavoritesView from '@/views/FavoritesView.vue'
+import AddRecipeView from '@/views/AddRecipeView.vue'
+import RecipeView from '@/views/RecipeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +13,19 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/favorites',
+      name: 'favorites',
+      component: FavoritesView,
+    },
+    {
+      path: '/add-recipe',
+      name: 'add-recipe',
+      component: AddRecipeView,
+    },
+    {
+      path: '/recipe/:id',
+      name: 'recipe',
+      component: RecipeView,
     },
   ],
 })
---
+++ section-11/vue/src/views/AddRecipeView.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Add Recipe</div>
+</template>
