e4307e6c9627a7120c22125adbcbdc85dbf07ebe
Garon <garon@vitrion.nl>
2025-07-31 14:07:29 +0200

    Finish up Section 11, set up Section 13

---
+++ section-13/vue/src/components/Child.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+interface Item {
+  id: number
+  name: string
+}
+
+defineProps<{ items: Item[] }>()
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in items" :key="item.id"><slot name="item" :item="item"></slot></li>
+  </ul>
+</template>
---
+++ section-13/vue/src/components/Parent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Child from '@/components/Child.vue'
+
+const items = ref([
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+])
+</script>
+
+<template>
+  <div><h1>Item List</h1></div>
+  <Child :items="items"
+    ><template #item="{ item }">
+      <strong class="">{{ item.name }}</strong>
+    </template></Child
+  >
+</template>
